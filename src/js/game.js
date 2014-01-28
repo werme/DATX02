a0188b1ef4c2d61c881b18b3c7a23e164b6535ef
Olle Werme <olle.werme@gmail.com>
2014-01-28 16:25:52 +0100

    Initial commit

---
+++ src/js/game.js
@@ -0,0 +1,30 @@
+(function() {
+  'use strict';
+
+  function Game() {
+    this.player = null;
+  }
+
+  Game.prototype = {
+
+    create: function () {
+      var x = this.game.width / 2
+        , y = this.game.height / 2;
+
+      this.player = this.add.sprite(x, y, 'player');
+      this.player.anchor.setTo(0.5, 0.5);
+    },
+
+    update: function () {
+      this.input.onDown.add(function() {
+        console.log(this);
+        console.log("Clicked!");
+      }, this);
+    }
+
+  };
+
+  window['kandidat'] = window['kandidat'] || {};
+  window['kandidat'].Game = Game;
+
+}());
---
+++ src/js/menu.js
@@ -0,0 +1,33 @@
+(function() {
+  'use strict';
+
+  function Menu() {
+    this.titleTxt = null;
+    this.startTxt = null;
+  }
+
+  Menu.prototype = {
+
+    create: function () {
+      var x = this.game.width / 2
+        , y = this.game.height / 2;
+
+      this.titleTxt = this.add.bitmapText(x, y, 'Click to start!', {font: '16px minecraftia', align: 'center'});
+      this.titleTxt.anchor.setTo(0.5, 0.5);
+
+      this.input.onDown.add(this.onDown, this);
+    },
+
+    update: function () {
+
+    },
+
+    onDown: function () {
+      this.game.state.start('game');
+    }
+  };
+
+  window['kandidat'] = window['kandidat'] || {};
+  window['kandidat'].Menu = Menu;
+
+}());

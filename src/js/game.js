34370bb0875ed9e13b7f4510a606eee898f0007e
Jesper Persson <jespert.persson@gmail.com>
2014-02-06 16:54:10 +0100

    Add controls for moving player and make camera follow

--- src/js/game.js
+++ src/js/game.js
@@ -3,23 +3,38 @@
 
   function Game() {
     this.player = null;
+    this.cursors = null;
   }
 
   Game.prototype = {
 
     create: function () {
+      this.game.world.setBounds(0,0, 1280, 940);
+      this.background = this.add.sprite(0,0, 'background');
+
       var x = this.game.width / 2
         , y = this.game.height / 2;
 
       this.player = this.add.sprite(x, y, 'player');
       this.player.anchor.setTo(0.5, 0.5);
+
+      this.cursors = this.game.input.keyboard.createCursorKeys();
+      this.game.camera.follow(this.player);
     },
 
     update: function () {
-      this.input.onDown.add(function() {
-        console.log(this);
-        console.log('Clicked!');
-      }, this);
+      if (this.cursors.up.isDown) {
+        this.player.y--;
+      } 
+      if (this.cursors.left.isDown) {
+        this.player.x--;
+      }
+      if (this.cursors.right.isDown) {
+        this.player.x++;
+      }
+      if (this.cursors.down.isDown) {
+        this.player.y++;
+      }
     }
 
   };

7349f6fa157c05624cf32eec7c11d22be6b8122c
Jesper Persson <jespert.persson@gmail.com>
2014-02-06 17:23:47 +0100

    Add animations for player movement

--- src/js/game.js
+++ src/js/game.js
@@ -17,6 +17,12 @@
 
       this.player = this.add.sprite(x, y, 'player');
       this.player.anchor.setTo(0.5, 0.5);
+      this.player.animations.add('walk-left', [8,9,10,11], 10, true);
+      this.player.animations.add('walk-right', [12,13,14,15], 10, true);
+      this.player.animations.add('walk-up', [0,1,2,3], 10, true);
+      this.player.animations.add('walk-down', [4,5,6,7], 10, true);
+
+      this.player.scale.setTo(3,3);
 
       this.cursors = this.game.input.keyboard.createCursorKeys();
       this.game.camera.follow(this.player);
@@ -24,17 +30,22 @@
 
     update: function () {
       this.player.body.velocity.setTo(0,0);
+
       if (this.cursors.up.isDown) {
         this.player.body.velocity.y = -100;
+        this.player.animations.play('walk-up');
       } 
       if (this.cursors.left.isDown) {
         this.player.body.velocity.x = -100;
+        this.player.animations.play('walk-left');
       }
       if (this.cursors.right.isDown) {
         this.player.body.velocity.x = 100;
+        this.player.animations.play('walk-right');
       }
       if (this.cursors.down.isDown) {
         this.player.body.velocity.y = 100;
+        this.player.animations.play('walk-down');
       }
     }
 

0b1d37b2d523b3f8bf9296158031e0d4ab858458
FannyM <alex.malmek@hotmail.com>
2014-02-25 17:39:03 +0100

    resolve merge conflict in helper.js

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -11,10 +11,25 @@ Darwinator.Helpers = {
         indexes[i][l] = layer[i][l].index;  
       }
     }
+    return indexes;
+  },
 
-  };
+  pixelsToTile: function(x, y) {
+    var pos = [];
+    if(x < 0) x = 0;
+    if(y < 0) y = 0;
+    pos[0] = Math.floor(x / window.Darwinator.GlobalValues.tileWidth);
+    pos[1] = Math.floor(y / window.Darwinator.GlobalValues.tileHeight);
+    return pos;
+  },
 
-  window.Darwinator = window.Darwinator || {};
-  window.Darwinator.Helpers = Helpers;
+  tileToPixels: function(x, y) {
+    var pos = [];
+    var tileWidth = window.Darwinator.GlobalValues.tileWidth;
+    var tileHeight = window.Darwinator.GlobalValues.tileHeight;
+    pos[0] = Math.floor(x * tileWidth + (tileWidth / 2));
+    pos[1] = Math.floor(y * tileHeight + (tileHeight / 2));
+    return pos;
+  }
 
 };

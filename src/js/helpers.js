ca4bf04bfa43a613dbafdf99cce3e5cee42fa250
FannyM <alex.malmek@hotmail.com>
2014-02-25 15:31:56 +0100

    Make sure negative values round to 0

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -11,23 +11,10 @@ Darwinator.Helpers = {
         indexes[i][l] = layer[i][l].index;  
       }
     }
-    return indexes;
-  },
 
-  pixelsToTile: function(x, y) {
-    var pos = [];
-    pos[0] = Math.floor(x / window.Darwinator.GlobalValues.tileWidth);
-    pos[1] = Math.floor(y / window.Darwinator.GlobalValues.tileHeight);
-    return pos;
-  },
+  };
 
-  tileToPixels: function(x, y) {
-    var pos = [];
-    var tileWidth = window.Darwinator.GlobalValues.tileWidth;
-    var tileHeight = window.Darwinator.GlobalValues.tileHeight;
-    pos[0] = Math.floor(x * tileWidth + (tileWidth / 2));
-    pos[1] = Math.floor(y * tileHeight + (tileHeight / 2));
-    return pos;
-  }
+  window.Darwinator = window.Darwinator || {};
+  window.Darwinator.Helpers = Helpers;
 
 };

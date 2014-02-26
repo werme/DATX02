7d804088e8a14d9e2c01ea0715b053501be2c0aa
Jesper Persson <jespert.persson@gmail.com>
2014-02-26 15:52:45 +0100

    Add folder for GA, specify final variables

---
+++ src/js/GA/gen_alg.js
@@ -0,0 +1,15 @@
+'use strict'
+
+window.Darwinator.GeneticAlgorithm = || {
+
+    NUMBER_OF_GENES         = 60;
+    CROSSOVER_PROBABILITY   = 0.8;
+    MUTATION_PROBABILITY    = 0.025;
+    TOURNAMENT_PARAMETER    = 0.75;
+    VARIABLE_RANGE          = 5.0;        // Might not need later?
+    NUMBER_OF_GENERATIONS   = 100;
+    NUMBER_OF_VARIABLES     = 2;
+    TOURNAMENT_SIZE         = 4;
+    ELITISM_DEGREE          = 1;
+
+};

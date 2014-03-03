edd54bf6144a49f4970215a7a1a3e8d1c8fbbf01
Jesper Persson <jespert.persson@gmail.com>
2014-03-03 14:25:47 +0100

    Enemies now only update path if player has moved

--- src/js/entities/enemy.js
+++ src/js/entities/enemy.js
@@ -4,28 +4,36 @@ Darwinator.Enemy = function(game, target, x, y, health) {
   Darwinator.Entity.call(this, game, x, y, health, 'enemy');
   this.scale.setTo(0.25,0.25);
   this.target = target;
-  this.path = null;
+  this.path = [];
   this.speed = 75;
   this.damage = 5;
   this.attacking = false;
   this.time = null;
   this.overlap = null;
+  this.counter = 0;       // Frame-counter
 };
 
 Darwinator.Enemy.prototype = Object.create(Darwinator.Entity.prototype);
 
 Darwinator.Enemy.prototype.update = function() {
   var currTile = Darwinator.Helpers.pixelsToTile(this.body.x, this.body.y);
+  var targetTile = Darwinator.Helpers.pixelsToTile(this.target.body.x, this.target.body.y);
 
-  // Changed the y target tile to be not freak out when player now overlaps with objects.
-  var targetTile = Darwinator.Helpers.pixelsToTile(this.target.body.x, this.target.body.y + 28);
-  Darwinator.Pathfinder.findPath(currTile[0], currTile[1], targetTile[0], targetTile[1], function(path){
-    this.path = path;
-  }.bind(this));
-  Darwinator.Pathfinder.calculate();
+  var pathLength = this.path.length;
+  if(!(pathLength && this.path[pathLength - 1].x === targetTile[0] && this.path[pathLength - 1].y === targetTile[1])) {
+    Darwinator.Pathfinder.findPath(currTile[0], currTile[1], targetTile[0], targetTile[1], function(path){
+      this.path = path;
+    }.bind(this));
+    Darwinator.Pathfinder.calculate();
+  }
 
-  if (this.path !== null) {
+  if (this.path.length) {
     var targetPos = Darwinator.Helpers.tileToPixels(this.path[1].x, this.path[1].y);
+    var distance = Darwinator.Helpers.calculateDistance(targetPos, [this.x, this.y]);
+    if (distance < 5 && this.path.length > 2) {      // Trial and error - modify if need be.
+      this.path.splice(0,1); // Remove first tile in path.
+      targetPos = Darwinator.Helpers.tileToPixels(this.path[1].x, this.path[1].y);
+    }
     this.game.physics.moveToXY(this, targetPos[0], targetPos[1], this.speed);
     if (this.path.length < 5 && this.currBreath > 1) {
       this.body.velocity.multiply(2,2);
@@ -33,6 +41,8 @@ Darwinator.Enemy.prototype.update = function() {
     } else if (this.currBreath < this.stamina) {
       this.currBreath += 0.2;
     }
+  } else {
+
   }
 
   // Target (ie. player) takes damage while the target and enemy overlap.
@@ -45,4 +55,6 @@ Darwinator.Enemy.prototype.update = function() {
   } else if (!this.overlap || ((this.game.time.time - this.time) > 250)) {
     this.attacking = false;
   }
+
+  this.counter++;
 };

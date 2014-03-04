10ee94aee23aca616e54cff533cc8b074e07cd56
FannyM <alex.malmek@hotmail.com>
2014-03-04 10:21:57 +0100

    spawns enemies according to tiles

--- src/js/game.js
+++ src/js/game.js
@@ -2,7 +2,7 @@
 
 Darwinator.GameState = function() {
   this.player   = null;
-  this.enemies    = null;
+  this.enemies  = null;
   this.cursors  = null;
   this.map      = null;
   this.tileset  = null;
@@ -10,8 +10,9 @@ Darwinator.GameState = function() {
   this.fps      = null;
   this.stats    = null;
   this.health   = null;
+  this.pauseText = null;
   this.spawnPositions = [];
-  this.numberOfEnemies = 20;
+  this.numberOfEnemies = 10;
 }
 
 Darwinator.GameState.prototype = {
@@ -44,7 +45,7 @@ Darwinator.GameState.prototype = {
     this.gameOver.fixedToCamera = true;
 
     Darwinator.Pathfinder = new EasyStar.js();
-    Darwinator.Pathfinder.enableDiagonals();
+    //Darwinator.Pathfinder.enableDiagonals();
     var indexes = Darwinator.Helpers.convertTileMap(this.map.layers[0].data);
     Darwinator.Pathfinder.setGrid(indexes);
     Darwinator.Pathfinder.setAcceptableTiles([1337, 168, 156, 157, 158, 172, 173, 174, 188, 189, 190, 205]);
@@ -79,24 +80,24 @@ Darwinator.GameState.prototype = {
     for (var i = 0; i < spawnIndexes.length; i++) {
       spawnIndexes[i] = i;
     }
-
-    this.enemies = this.game.add.group();
+    
     var rInd;
     var pos;
-    while (this.numberOfEnemies-- && spawnIndexes.length) {
+    while (this.numberOfEnemies && spawnIndexes.length) {
       rInd = Math.round(Math.random() * spawnIndexes.length -1);
       pos = spawnIndexes.splice(rInd,1);
       this.enemies.add(new Darwinator.Enemy(this.game, this.player, 
         this.spawnPositions[pos][0], 
         this.spawnPositions[pos][1], 100));
+      this.numberOfEnemies--;
     }
-
-    console.log("duurp");
   },
 
   initSpawnPosition: function () {
     var matrix = Darwinator.Helpers.convertTileMap(this.map.layers[0].data);
     
+    this.enemies = this.game.add.group();
+
     for (var i = 0; i < matrix.length; i++) {
       for(var j = 0; j < matrix[i].length; j++) {
         if (matrix[i][j] === 168){
@@ -119,10 +120,6 @@ Darwinator.GameState.prototype = {
     this.fps.content = 'FPS: ' + this.game.time.fps;
     this.stats.content = 'Player stamina: ' + Math.round(this.player.currBreath) + '/' + this.player.stamina;
     this.health.content = 'Health: ' + this.player.health;
-
-    if (this.numberOfEnemies){
-      this.spawnEnemies();
-    }
 
   },
 

9cf72f07946b74c93d4fe242fd8bd9d36a1779d1
Jesper Persson <jespert.persson@gmail.com>
2014-03-10 16:43:08 +0100

    Make helpers return and take tuples

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -29,15 +29,17 @@ Darwinator.Helpers = {
   * @return {Array} - The indexes of the tile, given as [x,y]
   */
   pixelsToTile: function(x, y) {
-    var pos = [];
     if (x < 0) {
       x = 0;
     }
     if (y < 0) {
       y = 0;
     }
-    pos[0] = Math.floor(x / Darwinator.TILE_WIDTH);
-    pos[1] = Math.floor(y / Darwinator.TILE_HEIGHT);
+    var pos = {
+      x: Math.floor(x / Darwinator.TILE_WIDTH),
+      y: Math.floor(y / Darwinator.TILE_HEIGHT)
+    }
+
     return pos;
   },
 
@@ -49,24 +51,28 @@ Darwinator.Helpers = {
   * @return {Array} - The pixel in the center of the tile, returned as [X,Y]
   */
   tileToPixels: function(x, y) {
-    var pos = [];
+    var pos;
     var tileWidth = Darwinator.TILE_WIDTH;
     var tileHeight = Darwinator.TILE_HEIGHT;
-    pos[0] = Math.floor(x * tileWidth + (tileWidth / 2));
-    pos[1] = Math.floor(y * tileHeight + (tileHeight / 2));
+    pos.x = Math.floor(x * tileWidth + (tileWidth / 2));
+    pos.y = Math.floor(y * tileHeight + (tileHeight / 2));
     return pos;
   },
 
   /**
   * Calculates distance between two points. Works for both distance in pixels and distance in tiles.
   * @method Darwinator.Helpers#calculateDistance
-  * @param {Array} - [pos1] The first point, given as [x,y]
-  * @param {Array} - [pos2] The second point, given as [x,y]
+  * @param {Array/Tuple} - [pos1] The first point, given as [x,y] or as an object with properties x,y
+  * @param {Array/Tple} - [pos2] The second point, given as [x,y] or as an object with properties x,y
   * @return {Number} The distance between the two points.
   */
   calculateDistance: function(pos1, pos2) {
-    var dx = pos1[0] - pos2[0];
-    var dy = pos1[1] - pos2[1];
+    var x1 = pos1.hasOwnProperty('x') ? pos1.x : pos1[0];
+    var y1 = pos1.hasOwnProperty('y') ? pos1.y : pos1[1];
+    var x2 = pos2.hasOwnProperty('x') ? pos2.x : pos2[0];
+    var y2 = pos2.hasOwnProperty('y') ? pos2.y : pos2[1];
+    var dx = x1 - x2;
+    var dy = y1 - y2;
     return Math.sqrt(dx*dx + dy*dy);
   }
 };

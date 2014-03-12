abc4788895866d8ad6663d090c4e84ffe42331b7
Olle Werme <olle.werme@gmail.com>
2014-03-12 10:01:12 +0100

    Add new player graphics

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -18,8 +18,7 @@ Darwinator.Preloader.prototype = {
     this.load.tilemap('level1', 'assets/level2.json', null, Phaser.Tilemap.TILED_JSON);
     this.game.load.image('tiles', 'assets/landscape.png');
     this.load.image('sword','assets/sword.png');
-    
-    this.load.spritesheet('player', 'assets/gal.png', 16, 16, 16);
+    this.load.spritesheet('player', 'assets/darwin.png', 32, 32, 16);
   },
 
   create: function () {

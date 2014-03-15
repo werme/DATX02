719df671127b1563c286961baa27784534cc78e9
Olle Werme <olle.werme@gmail.com>
2014-03-15 13:56:07 +0100

    Update bitmap text to new 2.0 format

--- src/js/menu.js
+++ src/js/menu.js
@@ -11,8 +11,9 @@ Darwinator.Menu.prototype = {
     var x = this.game.width / 2
       , y = this.game.height / 2;
 
-    this.titleTxt = this.add.bitmapText(x, y, 'Click to start!', {font: '16px minecraftia', align: 'center'});
-    this.titleTxt.anchor.setTo(0.5, 0.5);
+    this.titleTxt = this.add.bitmapText(x, y, 'minecraftia', 'Click to start!', 16);
+    this.titleTxt.align = 'center';
+    // this.titleTxt.anchor.setTo(0.5, 0.5);
 
     this.input.onDown.add(this.onDown, this);
   },

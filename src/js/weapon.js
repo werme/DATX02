41472e9bff044e65b04011257ced5f732ce01cac
Olle Werme <olle.werme@gmail.com>
2014-03-16 12:06:03 +0100

    Update namespace to currently used convention

--- src/js/weapon.js
+++ src/js/weapon.js
@@ -1,42 +1,36 @@
-(function() {
-  'use strict';
+'use strict';
 
-  function Weapon(game, x, y, coolDown, bulletSpeed, bullets, damage) {
-    this.game = game;
-    this.x = x;
-    this.y = y;
-    this.coolDown = coolDown;
-    this.nextFire = 0;
-    this.bullets = bullets;
-    this.bulletSpeed = bulletSpeed;
-    this.damage = damage;
+Darwinator.Weapon = function (game, x, y, coolDown, bulletSpeed, bullets, damage) {
+  this.game = game;
+
+  this.x           = x;
+  this.y           = y;
+  this.coolDown    = coolDown;
+  this.nextFire    = 0;
+  this.bullets     = bullets;
+  this.bulletSpeed = bulletSpeed;
+  this.damage      = damage;
+}
+
+Darwinator.Weapon.prototype.updateManually = function (x, y) {
+  this.x = x;
+  this.y = y;
+  this.game.physics.angleToPointer(this);
+
+  if (this.game.input.activePointer.isDown) {
+    this.fire();
   }
+};
 
-  Weapon.prototype = {
-    updateManually: function(x, y){
-      this.x = x;
-      this.y = y;
-      this.game.physics.angleToPointer(this);
-      if (this.game.input.activePointer.isDown){
-        this.fire();
-      }
-    },
+Darwinator.Weapon.prototype.fire = function() {
+  if (this.game.time.now > this.nextFire && this.bullets.countDead() > 0) {
+    this.nextFire = this.game.time.now + this.coolDown;
+    var bullet    = this.bullets.getFirstDead();
+    this.resetBullet(bullet);
+  }
+};
 
-    fire: function(){
-      if (this.game.time.now > this.nextFire && this.bullets.countDead() > 0){
-          this.nextFire = this.game.time.now + this.coolDown;
-          var bullet    = this.bullets.getFirstDead();
-          this.resetBullet(bullet);
-      }
-    },
-
-    resetBullet: function(bullet){
-      bullet.reset(this.x, this.y); // resets sprite and body
-      bullet.rotation = this.game.physics.moveToPointer(bullet, this.bulletSpeed);
-    }
-  };
-
-  window.Darwinator = window.Darwinator || {};
-  window.Darwinator.Weapon = Weapon;
-
-}());
+Darwinator.Weapon.prototype.resetBullet = function (bullet) { 
+  bullet.reset(this.x, this.y); // resets sprite and body
+  bullet.rotation = this.game.physics.moveToPointer(bullet, this.bulletSpeed);
+};

a23cd3b311098ba990a4012b56f3f4981a5acf09
FannyM <alex.malmek@hotmail.com>
2014-03-17 15:38:14 +0100

    corrected constructor call of player

--- src/js/game.js
+++ src/js/game.js
@@ -111,7 +111,7 @@ Darwinator.GameState.prototype = {
   spawnPlayer: function (x, y) {
     // Instanciate new player or reset existing.
     if (!this.game.player) {
-      this.game.player = new Darwinator.Player(this.game, x, y, this.cursors, Darwinator.PLAYER_BASE_HEALTH, 1, 1, 1);
+      this.game.player = new Darwinator.Player(this.game, x, y, this.cursors);
     } else {
       this.game.player.reset(x, y, Darwinator.PLAYER_BASE_HEALTH);
       this.game.player.bringToTop();

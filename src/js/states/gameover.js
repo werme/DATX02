f64f7fff292a750d80967ce7a2cad2b83b9b0064
Olle Werme <olle.werme@gmail.com>
2014-03-31 19:52:03 +0200

    Add state for game over screen

---
+++ src/js/states/gameover.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Darwinator.GameOverScreen = function() {
+    this.container = null;
+};
+
+Darwinator.GameOverScreen.prototype = {
+
+    create: function () {},
+
+    update: function () {},
+
+};

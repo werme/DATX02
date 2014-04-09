37330cc6911f13421d3b53946e1021f642cc6a83
David Grankvist <grankvist.david@gmail.com>
2014-04-09 20:22:46 +0200

    make sure no potential revived enemies are still dodging

--- src/js/entities/enemy.js
+++ src/js/entities/enemy.js
@@ -113,6 +113,8 @@ Darwinator.Enemy.prototype.update = function() {
 
   if (this.health <= 0 && this.alive){
     this.game.time.events.remove(this.dodgeTimer);
+    this.dodging = false;
+    this.alpa = 1;
     console.log('%c Enemy killed by player! ', 'background: black; color: orange');
     this.kill();
   }

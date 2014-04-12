aba8400750d8b5023472bd18f32ee3a2308366b6
Olle Werme <olle.werme@gmail.com>
2014-04-12 14:15:01 +0200

    Fix player attribute reset after wave end

--- src/js/states/game.js
+++ src/js/states/game.js
@@ -153,11 +153,13 @@ Darwinator.GameState.prototype = {
 
     spawnPlayer: function (x, y) {
         // Instanciate new player or reset existing
-        if (!this.game.player) {
-            this.game.player = new Darwinator.Player(this.game, x, y, this.cursors);
-        } else {
+        if (this.game.player) {
+            if (this.game.player.health <= 0) {
+                this.game.player.resetAttributes();
+            }
             this.game.player.reset(x, y, Darwinator.PLAYER_BASE_HEALTH);
-            this.game.player.resetAttributes();
+        } else {
+            this.game.player = new Darwinator.Player(this.game, x, y, this.cursors);
         }
 
         // Add player sprite to stage and focus camera
